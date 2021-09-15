f7168898632956f4290524381b6f63eaf4af6866
jitendra kumar <jitendra199618@gmail.com>
2021-09-15 12:30:27 +0530

    another logic to check 2nd string

--- anagram.js
+++ anagram.js
@@ -1,16 +1,26 @@
-const validAnagram = (first,second) => {
-    if(first.length !== second.length){
+const validAnagram = (first, second) => {
+    if (first.length !== second.length) {
         return false;
     }
     const firstObj = charCount(first);
-    const secondObj = charCount(second);
+    
+    // const secondObj = charCount(second);
 
-    for (const key in firstObj){
-        if(!secondObj.hasOwnProperty(key)){
-            return false ;
-        }
-        if(firstObj[key] !== secondObj[key]){
+    // for (const key in firstObj){
+    //     if(!secondObj.hasOwnProperty(key)){
+    //         return false ;
+    //     }
+    //     if(firstObj[key] !== secondObj[key]){
+    //         return false;
+    //     }
+    // }
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        if (!firstObj[letter]) {
             return false;
+        } else {
+            firstObj[letter]--;
         }
     }
     return true;
@@ -21,20 +31,20 @@ const charCount = str => {
     let strLower = str.toLowerCase();
     let obj = {};
     let specialChar = /^[a-z]+$/;
-    for(let i = 0 ; i < strLower.length ; i++){
-        if(obj[strLower[i]]>0){
+    for (let i = 0; i < strLower.length; i++) {
+        if (obj[strLower[i]] > 0) {
             obj[strLower[i]]++;
-        }else{
-            if(specialChar.test(strLower[i])){
-                obj[strLower[i]] = 1 ;
+        } else {
+            if (specialChar.test(strLower[i])) {
+                obj[strLower[i]] = 1;
             }
         }
     }
-    return obj ;
+    return obj;
 }
 
 
 //console.log(charCount('asjdjaaasdjkjdej'));
 
-console.log(validAnagram('aaz','zza'));
-console.log(validAnagram('anagram','nagaram'));
+console.log(validAnagram('aaz', 'zza'));
+console.log(validAnagram('anagram', 'nagaram'));
