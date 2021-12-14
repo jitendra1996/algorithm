98468bf020594e798ec598c3975efe1c7027ae0f
jitendra kumar <jitendra199618@gmail.com>
2021-12-14 21:55:55 +0530

    little tweak in bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,14 +1,18 @@
 const bubbleSort = (arr) => {
+    let noSwap;
   for (let i = 0; i < arr.length; i++) {
+        noSwap = true;
     for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwap = false;
       }
     }
+    if(noSwap) break;
   }
   return arr;
 };
 
-console.log(bubbleSort([37, 45, 12, 8, 2]));
+console.log(bubbleSort([45,2,8,12,37]));
